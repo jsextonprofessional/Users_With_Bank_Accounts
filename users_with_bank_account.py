80bbf7ddf663557bcbf634af5b1a15c84cc333c0
jsprofessional <jshuman@protonmail.com>
2021-07-08 20:02:40 -0500

    added account to account.balance in user class methods

--- users_with_bank_account.py
+++ users_with_bank_account.py
@@ -1,27 +1,5 @@
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-        self.account = BankAccount(int_rate = .02, balance = 0)
-
-    def make_deposit(self, amount):
-        self.account += amount
-        return self
-
-    def make_withdrawal(self, amount):
-        self.account -= amount
-        return self
-
-    def display_user_balance(self):
-        print(f"Name: {self.name}, {self.account}")
-        return self
-
-    def transfer_money(self, other_user, amount):
-        self.account -= amount
-        other_user.account += amount
-        return self
-        
 class BankAccount:
+
     def __init__(self, int_rate = .02, balance = 0):
         self.int_rate = int_rate
         self.balance = balance
@@ -41,6 +19,32 @@ class BankAccount:
     def yield_interest(self):
         self.balance *= self.int_rate
         return self
+
+
+class User:
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate = .02, balance = 0)
+
+    def make_deposit(self, amount):
+        self.account.balance += amount
+        return self
+
+    def make_withdrawal(self, amount):
+        self.account.balance -= amount
+        return self
+
+    def display_user_balance(self):
+        print(f"Name: {self.name}, {self.account}")
+        return self
+
+    def transfer_money(self, other_user, amount):
+        self.account.balance -= amount
+        other_user.account.balance += amount
+        return self
+        
 
 first_user = User("Guido van Rossum", "guido@python.com" )
 second_user = User("Monty Python", "monty@python.com")
